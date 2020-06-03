c750f44256fd46c375cab1c77cb142af6c67793b
j m <jm@js-macbook-pro.home>
2020-06-03 15:53:39 +0200

    system tests

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -1,11 +1,13 @@
 require "application_system_test_case"
 
 class SimpleNavigationsTest < ApplicationSystemTestCase
-  # test "search term is displayed" do
-  #   visit simple_navigations_url
-  
-  #   assert_selector "h1", text: "SimpleNavigation"
-  # end
+test 'search term is diplayed' do
+  visit('http://localhost:3000/')
+  assert page.has_content?('Bucket List')
+  fill_in('q', with: 'Spain').send_keys(:return)
+  assert has_content?('Spain')
+  assert current_url.include?('q=Spain')
+end
 
   test "style guide navigation" do
     visit ('http://localhost:3000/')
@@ -18,7 +20,6 @@ class SimpleNavigationsTest < ApplicationSystemTestCase
     click_on('Organisms')
     assert page.has_content?('Create goal')
     click_link('My Bucket List', match: :first)
-    sleep(5.seconds)
     assert page.has_content?('Some ideas')
   end
 end
