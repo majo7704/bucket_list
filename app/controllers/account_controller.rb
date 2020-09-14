e0820c4d444538a205a56a9616c74fd289ad8498
j m <jm@js-macbook-pro.home>
2020-09-14 13:23:26 +0200

    declares helper method current user in account controller

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,14 +1,18 @@
 class AccountController < ApplicationController
   before_action :ensure_authenticated
 
-  def ideas
-    @ideas = Idea.all
-  
-  def ensure_authenticated
-    unless(logged_in?)
-      redirect_to login_path
-    end 
-  end  
+  helper_method :current_user
 
+  def ideas
+    user = User.find(session[:user_id])
+    @ideas = user.ideas
+  end
+
+  def ensure_authenticated
+    redirect_to login_path unless(logged_in?)
+  end 
   
+  def current_user
+    User.find(session[:user_id])
+  end
 end
