bd6781b466ee043fbc02f38ffe7810acff108c17
j m <jm@js-macbook-pro.home>
2020-09-15 10:10:12 +0200

    move current_user helper method to application controller

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,7 +1,6 @@
 class AccountController < ApplicationController
   before_action :ensure_authenticated
 
-  helper_method :current_user
 
   def edit
   end
@@ -17,17 +16,14 @@ class AccountController < ApplicationController
   end
 
   def goals
-    @goals=current_user.goals
+    @goals = current_user.goals
   end  
 
   def ensure_authenticated
     redirect_to login_path unless(logged_in?)
   end 
   
-  def current_user
-    User.find(session[:user_id])
-  end
-
+  
   private
   def user_params
     params.require(:user).permit(:email, :name, :avatar_url)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def can_edit?(goal)
+    current_user == goal.user
+  end 
 end
