9e77bc4f7ed39605c2ea5e37916c91f6ea55c8b3
j m <jm@js-macbook-pro.home>
2020-09-21 17:52:33 +0200

    create js view created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,12 @@ class CommentsController < ApplicationController
 before_action :ensure_authenticated
 
 def create 
-  comment = Comment.new comment_params
+  @comment = Comment.new comment_params
   idea = Idea.find(params[:idea_id])
-  comment.idea = idea
+  @comment.idea = idea
   #associate comment with a user
-  comment.user = current_user
-  comment.save!
-  redirect_to idea_path(idea)
-
+  @comment.user = current_user
+  @comment.save!
 end
 
 private
