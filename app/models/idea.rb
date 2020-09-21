66f61061d291a511ca0b56f8d500c67bb478e4c0
j m <jm@js-macbook-pro.home>
2020-09-21 11:49:30 +0200

    adds title_contains scope

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -9,7 +9,7 @@ class Idea < ApplicationRecord
   has_and_belongs_to_many :users
 
   scope :most_recent, -> { order(created_at: :desc).limit(3)}
-  
+  scope :title_contains, ->(term) { where('title LIKE ?', "%#{term}%") }
 
   def self.search(search_term)
     wildcard_filter = "%#{search_term}%"
