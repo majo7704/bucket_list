c2c3322410f534b85890985c26919ec1e5ad3826
j m <jm@js-macbook-pro.home>
2020-09-27 19:44:55 +0200

    includes links to select locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
 
   helper_method :logged_in?, :current_user
 
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default.locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def ensure_authenticated
     redirect_to login_path unless(logged_in?)
   end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,39 @@
 Rails.application.routes.draw do
+  root to:'home#index'
+  scope '/:locale' do
   
-root to:'home#index'
+    root to:'home#index'
 
-get 'home/index'
+    get 'home/index'
 
-get 'styles/atoms'
+    get 'styles/atoms'
 
-get 'styles/molecules'
+    get 'styles/molecules'
 
-get 'styles/organisms'
+    get 'styles/organisms'
 
-resources :sessions, only: [:new, :create, :destroy]
+    resources :sessions, only: [:new, :create, :destroy]
 
-resources :users, only: [:new, :create, :edit, :update] do
-  resources :goals
-end 
+    resources :users, only: [:new, :create, :edit, :update] do
+      resources :goals
+    end 
 
-resources :ideas do
-  resources :comments
-  resources :photos
-end 
+    resources :ideas do
+      resources :comments
+      resources :photos
+    end 
 
-get 'account', to: 'account#edit'
-patch 'account', to: 'account#update'
-get 'account/ideas'
-get 'account/goals'
+    get 'account', to: 'account#edit'
+    patch 'account', to: 'account#update'
+    get 'account/ideas'
+    get 'account/goals'
 
-get 'login', to: 'sessions#new'
+    get 'login', to: 'sessions#new'
 
-get 'signup', to: 'users#new'
+    get 'signup', to: 'users#new'
 
-  # get 'complete/style/guide', to:'styles#atoms'
+    # get 'complete/style/guide', to:'styles#atoms'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
